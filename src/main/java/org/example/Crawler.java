a365d51314e1dd17100f2bed1d6db8df4dfdf870
a.krapilskiy <a.krapilskiy@group-ib.com>
2022-12-15 03:59:38 +0300

    lint

--- src/main/java/org/example/Crawler.java
+++ src/main/java/org/example/Crawler.java
@@ -26,47 +26,47 @@ public class Crawler {
 //    [0] - Начальный сайт
 //    [1] - Максимальная глубина поиска
 //    [2] - Максимальная количество worker'ов
-public static void main(String[] args) {
-//    args = new String[]{"https://en.wikipedia.org/wiki/Wiki", "1", "10"};
-    args = new String[]{"https://mtuci.ru", "2", "25"};
-//    args = new String[]{"https://stackoverflow.com/questions/5244782/java-concurrent-queries", "2", "50"};
-//    args = new String[]{"https://habr.com/", "2", "10"};
+    public static void main(String[] args) {
+    //    args = new String[]{"https://en.wikipedia.org/wiki/Wiki", "1", "10"};
+    //    args = new String[]{"https://mtuci.ru", "2", "25"};
+    //    args = new String[]{"https://stackoverflow.com/questions/5244782/java-concurrent-queries", "2", "50"};
+        args = new String[]{"https://habr.com/", "2", "50"};
 
-    if (args.length == 3) {
-        String startUrl = args[0];
-        int maxDepth = Integer.parseInt(args[1]);
-        int numThreads = Integer.parseInt(args[2]);
+        if (args.length == 3) {
+            String startUrl = args[0];
+            int maxDepth = Integer.parseInt(args[1]);
+            int numThreads = Integer.parseInt(args[2]);
 
-        URLPool pool = new URLPool(maxDepth);
+            URLPool pool = new URLPool(maxDepth);
 
-        try {
-            pool.addPair(new URLDepthPair(startUrl, 0));
-        } catch (MalformedURLException e) {
-            System.out.println("Invalid start URL");
-        }
-
-        createWorkers(pool, numThreads);
-
-//        данная часть кода отвечает за то, что бы во время остановить выполнение программы
-//        идея заключается в том, что если очередь пуста
-//        (то есть количество ожидающих задания потоков равно их количеству)
-//        то программа заканчивает работу и выводит результаты
-        while (pool.getWait() != numThreads) {
             try {
-                Thread.sleep(10);
-            } catch (InterruptedException ignored) {
+                pool.addPair(new URLDepthPair(startUrl, 0));
+            } catch (MalformedURLException e) {
+                System.out.println("Invalid start URL");
             }
-        }
 
-        try {
-            showResult(pool.getResult());;
-        } catch (NullPointerException ignored) {
-        }
-        System.exit(0);
+            createWorkers(pool, numThreads);
 
-    } else {
-        System.out.println("usage: java Crawler <URL> <maximum_depth> <num_threads> or second/third not digit");
+    //        данная часть кода отвечает за то, что бы во время остановить выполнение программы
+    //        идея заключается в том, что если очередь пуста
+    //        (то есть количество ожидающих задания потоков равно их количеству)
+    //        то программа заканчивает работу и выводит результаты
+            while (pool.getWait() != numThreads) {
+                try {
+                    Thread.sleep(10);
+                } catch (InterruptedException ignored) {
+                }
+            }
+
+            try {
+                showResult(pool.getResult());;
+            } catch (NullPointerException ignored) {
+            }
+            System.exit(0);
+
+        } else {
+            System.out.println("usage: java Crawler <URL> <maximum_depth> <num_threads> or second/third not digit");
+        }
     }
-}
 }
 
